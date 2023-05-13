3933c677b90b93f1837b5859bab77ab541bfa4c4
Amit-Saha98 <sahaamit120898@gmail.com>
2023-05-13 23:46:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [ip1,setip1] = useState();
+  const [ip2,setip2] = useState();
+  const [error,setError] = useState();
+  const [ans,setAns] = useState();
+  const[res,setRes]= useState(false);
+
+  const handle1 = (event)=>{
+    setip1(parseInt(event.target.value));
+  }
+
+  const handle2 = (event)=>{
+    setip2(parseInt(event.target.value));
+  }
+   
+
+  const calc = (type)=>{
+      
+
+      if(validate(ip1,ip2)) {
+        if(type==='+'){
+            setAns(ip1+ip2);
+        }
+        else if(type==='-'){
+          setAns(ip1-ip2);
+        }
+        else if(type==='*'){
+           setAns(ip1*ip2);
+      }
+      else{
+        setAns(ip1/ip2);
+      }
+      setRes(true);
+      }
+
+      else {
+        setError("enter valid input");
+      }
+  }
+
+  function validate(a,b){
+    if(a===isNaN||b===isNaN)return false;
+    return true;
+  }
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>
+        <h1>React Calculator</h1>
+        <input onChange={handle1} type='number'></input>
+        <br></br>
+        <input onChange={handle2} type='number'></input>
+        <br></br>
+        <button onClick={()=>calc('+')}>+</button>
+        <button onClick={()=>calc('-')}>-</button>
+        <button onClick={()=>calc('*')}>*</button>
+        <button onClick={()=>calc('/')}>/</button>
+        <p>Result = {ans}</p>
+        <p style={{color:"red"}}>{error}</p>
+        <p style={res===false ?{display:'none'}: {"display":"block",color:"green"}}>Success: Your result is shown above</p>
+     </div>
     </div>
   );
 }
